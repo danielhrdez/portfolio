15881d7773eb9be41c6463be2b08631a4a915dce
Daniel <alu0101331720@ull.edu.es>
2022-09-21 22:47:46 +0100

    updated csv lang files

--- scripts/makeLanguageJSON.ts
+++ scripts/makeLanguageJSON.ts
@@ -5,10 +5,19 @@ import getJSONFromCSV from "../src/utils/getJSONFromCSV";
  * @desc This function generates the language.json file.
  * @returns {void} This function returns nothing.
  */
-function makeLanguageJSON(): void {
-  const csv = fs.readFileSync('./src/data/language.csv', 'utf8');
+function makeLanguageJSON(input: string, output: string): void {
+  const csv = fs.readFileSync(input, 'utf8');
   const data = getJSONFromCSV(csv);
-  fs.writeFileSync('./src/data/language.json', JSON.stringify(data, null, 2));
+  fs.writeFileSync(output, JSON.stringify(data, null, 2));
 }
 
-makeLanguageJSON();
+function main(): void {
+  const input = './src/data/language.csv';
+  const output = './src/data/language.json';
+  makeLanguageJSON(input, output);
+  fs.watchFile(input, () => {
+    makeLanguageJSON(input, output);
+  });
+}
+
+main();
--- src/utils/getJSONFromCSV.ts
+++ src/utils/getJSONFromCSV.ts
@@ -22,16 +22,31 @@ interface ICSV {
 function getJSONFromCSV(csv: string): ICSV {
   const lines = csv.split('\n');
   const result: ICSV = {};
-  const headers = lines[0].split(',');
+  const headers = lines[0].split(',').slice(1);
   for (let i = 1; i < lines.length; i++) {
     const obj: ICSVRow = {};
-    const currentline = lines[i].split(',');
+    const [key, ...currentline] = CSVtoArray(lines[i]);
     for (let j = 0; j < headers.length; j++) {
       obj[headers[j]] = currentline[j];
     }
-    result[currentline[0].split(' ')[0]] = obj;
+    result[key] = obj;
   }
   return result;
 }
+
+function CSVtoArray(text: string): string[] {
+  const regexValid = /^\s*(?:'[^'\\]*(?:\\[\S\s][^'\\]*)*'|"[^"\\]*(?:\\[\S\s][^"\\]*)*"|[^,'"\s\\]*(?:\s+[^,'"\s\\]+)*)\s*(?:,\s*(?:'[^'\\]*(?:\\[\S\s][^'\\]*)*'|"[^"\\]*(?:\\[\S\s][^"\\]*)*"|[^,'"\s\\]*(?:\s+[^,'"\s\\]+)*)\s*)*$/;
+  const regexValue = /(?!\s*$)\s*(?:'([^'\\]*(?:\\[\S\s][^'\\]*)*)'|"([^"\\]*(?:\\[\S\s][^"\\]*)*)"|([^,'"\s\\]*(?:\s+[^,'"\s\\]+)*))\s*(?:,|$)/g;
+  if (!regexValid.test(text)) throw new Error('Invalid CSV');
+  const a = [];
+  text.replace(regexValue, (_, m1, m2, m3) => {
+    if (m1 !== undefined) a.push(m1.replace(/\\'/g, "'"));
+    else if (m2 !== undefined) a.push(m2.replace(/\\"/g, '"'));
+    else if (m3 !== undefined) a.push(m3);
+    return '';
+  });
+  if (/,\s*$/.test(text)) a.push('');
+  return a;
+};
 
 export default getJSONFromCSV;
