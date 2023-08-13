595cf2f7715d3894ea87a64efe3cc4b6d295eea0
Daniel Hernández de León <alu0101331720@ull.edu.es>
2023-08-14 00:03:11 +0100

    trying something in csp

--- svelte.config.js
+++ svelte.config.js
@@ -21,8 +21,7 @@ const config = {
 		},
 		csp: {
 			directives: {
-				'script-src': ['self', 'https://va.vercel-scripts.com/'],
-				'connect-src': ['self', 'https://va.vercel-scripts.com/']
+				'script-src': ['self', 'https://va.vercel-scripts.com/', 'https://vercel.live/']
 			}
 		}
 	}
